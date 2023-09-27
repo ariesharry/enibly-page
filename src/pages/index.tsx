62827b22278a3fc0f349cf209eb106d78527cbf6
ariesharry <ariesharry3@gmail.com>
2023-09-28 00:01:21 +0700

    comment unecessary component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,19 +6,19 @@ import { MainLayout } from '@/components/layout'
 
 const DynamicHomeHero = dynamic(() => import('../components/home/hero'))
 const DynamicHomeFeature = dynamic(() => import('../components/home/feature'))
-const DynamicHomePopularCourse = dynamic(() => import('../components/home/popular-courses'))
-const DynamicHomeTestimonial = dynamic(() => import('../components/home/testimonial'))
-const DynamicHomeOurMentors = dynamic(() => import('../components/home/mentors'))
+// const DynamicHomePopularCourse = dynamic(() => import('../components/home/popular-courses'))
+// const DynamicHomeTestimonial = dynamic(() => import('../components/home/testimonial'))
+// const DynamicHomeOurMentors = dynamic(() => import('../components/home/mentors'))
 const DynamicHomeNewsLetter = dynamic(() => import('../components/home/newsletter'))
 
 const Home: NextPageWithLayout = () => {
   return (
     <>
       <DynamicHomeHero />
-      <DynamicHomePopularCourse />
+      {/* <DynamicHomePopularCourse /> */}
       <DynamicHomeFeature />
-      <DynamicHomeTestimonial />
-      <DynamicHomeOurMentors />
+      {/* <DynamicHomeTestimonial /> */}
+      {/* <DynamicHomeOurMentors /> */}
       <DynamicHomeNewsLetter />
     </>
   )
