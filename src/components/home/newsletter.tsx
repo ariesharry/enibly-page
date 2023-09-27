7258ca3d52a0156c9694c914573b0d167aa4f72c
ariesharry <ariesharry3@gmail.com>
2023-09-28 00:38:06 +0700

    update package and newsletter

--- src/components/home/newsletter.tsx
+++ src/components/home/newsletter.tsx
@@ -1,13 +1,33 @@
-import React, { FC } from 'react'
-import Box from '@mui/material/Box'
-import InputBase from '@mui/material/InputBase'
-import Container from '@mui/material/Container'
-import Typography from '@mui/material/Typography'
-import { StyledButton } from '../styled-button'
+import React, { FC, useRef, useState  } from 'react';
+import emailjs from '@emailjs/browser';
+import Box from '@mui/material/Box';
+import InputBase from '@mui/material/InputBase';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';  // Import Button from MUI
 
 const HomeNewsLetter: FC = () => {
+  const form = useRef<HTMLFormElement>(null);  // Updated this line
+  const [email, setEmail] = useState('');  // Added this line
+  const [submitted, setSubmitted] = useState(false);  // Added this line
+
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>): void => {  // Updated this line
+    e.preventDefault();
+
+    if (form.current) {  // Updated this line
+      emailjs.sendForm('service_z5vzsbf', 'template_hlwlw5t', form.current, 'EMFzzp9u04woxW648')
+        .then((result) => {
+            console.log(result.text);
+            setEmail('');  // Added this line
+            setSubmitted(true);  // Added this line
+        }, (error) => {
+            console.log(error.text);
+        });
+    }
+  };
+
   return (
-    <Box sx={{ backgroundColor: 'background.paper', py: { xs: 8, md: 10 } }}>
+    <Box id='newsletter' sx={{ backgroundColor: 'background.paper', py: { xs: 8, md: 10 } }}>
       <Container>
         <Box
           sx={{
@@ -19,42 +39,71 @@ const HomeNewsLetter: FC = () => {
           }}
         >
           <Typography variant="h1" component="h2" sx={{ mb: 1, fontSize: { xs: 32, md: 42 } }}>
-            Subscribe to Our News Letter
+            Register for Beta Tester Now!
           </Typography>
-          <Typography sx={{ mb: 6 }}>Subscribe to our newsletter to get information about our courses.</Typography>
+          <Typography sx={{ mb: 6 }}>Be Among the First to Experience Enibly Bot</Typography>
 
-          <Box
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-              flexDirection: { xs: 'column', md: 'row' },
-              justifyContent: 'space-around',
-              width: { xs: '100%', md: 560 },
-              mx: 'auto',
-            }}
-          >
-            <InputBase
+          <form ref={form} onSubmit={sendEmail}>
+            <Box
               sx={{
-                backgroundColor: 'background.paper',
-                borderRadius: 3,
-                width: '100%',
-                height: 48,
-                px: 2,
-                mr: { xs: 0, md: 3 },
-                mb: { xs: 2, md: 0 },
+                display: 'flex',
+                alignItems: 'center',
+                flexDirection: { xs: 'column', md: 'row' },
+                justifyContent: 'space-around',
+                width: { xs: '100%', md: 560 },
+                mx: 'auto',
               }}
-              placeholder="Enter your Email Address"
-            />
-            <Box>
-              <StyledButton disableHoverEffect size="large">
-                Subscribe
-              </StyledButton>
+            >
+              <InputBase
+                name="user_email"  // Added name attribute
+                value={email}  // Added this line
+                onChange={(e) => setEmail(e.target.value)}
+                sx={{
+                  backgroundColor: 'background.paper',
+                  borderRadius: 3,
+                  width: '100%',
+                  height: 48,
+                  px: 2,
+                  mr: { xs: 0, md: 3 },
+                  mb: { xs: 2, md: 0 },
+                }}
+                placeholder="Enter your Email Address"
+              />
+              <Box>
+                <Button
+                  type="submit"
+                  size="large"
+                  sx={{
+                    backgroundColor: 'primary.main',
+                    color: 'white',
+                    borderRadius: '10px',
+                    height: '48px',  // Set height to match the entry form
+                    padding: '0 30px',
+                    '&:hover': {
+                      backgroundColor: 'primary.dark',
+                    },
+                    '&:active': {
+                      backgroundColor: 'primary.darker',
+                    },
+                    transition: 'background-color 0.3s',
+                  }}
+                >
+                  Submit
+                </Button>
+              </Box>
             </Box>
-          </Box>
+          </form>
+          {submitted && (
+            
+            <Typography sx={{ color: 'primary.main' }}>
+              <br /><b>Congratulations!</b> <br /><br /> You have registered as a Beta Tester for Enibly Bot. We will notify you by email when we are launching the Beta Tester Version.
+            </Typography>
+            
+          )}
         </Box>
       </Container>
     </Box>
-  )
-}
+  );
+};
 
-export default HomeNewsLetter
+export default HomeNewsLetter;
